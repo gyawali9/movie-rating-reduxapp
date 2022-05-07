380c21f081c9a0f72f7cae76dff661dc0662f917
gyawali9 <roshangyawali9@gmail.com>
2022-05-07 22:12:38 +0545

    adding routing to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import Footer from './components/Footer/Footer'
 import './App.scss'
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        
+        <Footer/>
+      </Router>
+    </div>
   )
 }
 
