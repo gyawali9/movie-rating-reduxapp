c0a3d57c58a1ee1d90f4bf5c52d61b89ea166eaf
gyawali9 <roshangyawali9@gmail.com>
2022-05-08 09:44:53 +0545

    api calls using axios

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from "../../common/apis/movieApi"
+import {APIKey} from "../../common/apis/MovieApiKey"
 import './Home.scss'
 const Home = () => {
+  useEffect(() => {
+    const movieText ="Harry"
+    const fetchMovies = async() =>{
+      const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`
+      )
+      .catch((err)=> {
+        console.log("Err :",err)
+      })
+      console.log("Response from API :", response)
+    }
+    fetchMovies();
+    
+  }, [])
+  
   return (
-    <div>Home</div>
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing/>
+    </div>
   )
 }
 
